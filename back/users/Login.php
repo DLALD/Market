c8adcc8be1e3a60d72115cc8f543dc5a2bb6e631
DLALD <luisalejandrolopezdavila@gmail.com>
2023-04-24 08:35:29 -0500

    ms

--- back/users/Login.php
+++ back/users/Login.php
@@ -2,5 +2,12 @@
 //login-Sign in
 include("../config/cnx_db.php");
 $email =$_POST["email"];
-$passw =$_POST["password"];
+$passw =md5($_POST["password"]);
 $sql ="SELECT * FROM users WHERE email ='$email' AND password ='passwd'";
+$result =$conn->query($sql);
+if($result->num_rows > 0){
+    echo"User has been found successfully.you're login";
+}else{
+    echo"Verify your date ";
+}
+?>
