cb521fd7c30d64dccd2b8fbf7db3d1bce2e49bc5
DLALD <luisalejandrolopezdavila@gmail.com>
2023-05-14 23:42:34 -0500

    ms

--- back/users/Login.php
+++ back/users/Login.php
@@ -1,21 +1,17 @@
 <?php
 //login-Sign in
 include("../config/cnx_db.php");
-$email =$_POST["email"];
-$passw =md5($_POST["password"]);
-$sql ="SELECT * FROM users WHERE email ='$email' AND password ='passwd'";
-$result =$conn->query($sql);
-if($result->num_rows > 0){
-    //echo"User has been found successfully.you're login";
-    //echo "User has been Create Successfully";
-    //header("Location:http://127.0.0.1/Market/Front/Login.html");
-    //echo "<script>alert('user has been Create Successfully' )</script>";
-    header("Location:http://127.0.0.1/Market/Front/Login.html");
-    
-   
-}else{
-    echo "<script>alert('user does't exist ')</script>";
-    header ("refresh=0;url=http://127.0.0.1/Market/Front/Login.html");
-    //echo"Verify your date ";
+$email = $_POST["email"];
+$password = md5($_POST["passwd"]);
+$sql = "SELECT * FROM users WHERE email = '$email' AND password = '$password'";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    // Usuario encontrado exitosamente. Realiza las acciones necesarias aquí.
+    header("Location: http://127.0.0.1/Market/Front/Login.html");
+    exit();
+} else {
+    echo "<script>alert('User doesn't exist')</script>";
+    header("refresh:0;url=http://127.0.0.1/Market/Front/Login.html");
+    exit();
 }
 ?>
--- back/users/create_user.php
+++ back/users/create_user.php
@@ -1,14 +1,110 @@
 <?php
 include_once("../config/cnx_db.php");
-$FirsName=$_POST['f_name'];
-$LastName =$_POST['l_name'];
-$email=$_POST['email'];
-$pass=md5($_POST['passwd']);
-$sql = " INSERT INTO users (Firs_Name,Last_Name,email,password) 
-values('$FirsName','$LastName','$email','$pass')";
-//$conn->query($sql);
-if($conn->query($sql)===TRUE){
-  echo "User has been Create Successfully";
-}else{
-    echo "Error:"-$conn->error;
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $Firstname = $_POST['f_name'];
+    $Lastname = $_POST['l_name'];
+    $email = $_POST['email'];
+    $password = md5($_POST['passwd']);
+
+    $sql = "INSERT INTO users (Firs_Name, Last_Name, email, password) 
+            VALUES ('$Firstname', '$Lastname', '$email', '$password')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "User has been created successfully.";
+    } else {
+        echo "Error: " . $conn->error;
+    }
 }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Create User</title>
+    <style>
+        body {
+            background-color: #f2f2f2;
+            font-family: Arial, sans-serif;
+        }
+
+        form {
+            margin: 20px auto;
+            width: 300px;
+            padding: 20px;
+            background-color: white;
+            border-radius: 5px;
+            box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.2);
+        }
+
+        label {
+            display: block;
+            font-weight: bold;
+            margin-bottom: 5px;
+        }
+
+        input[type="text"], input[type="email"], input[type="password"] {
+            width: 100%;
+            padding: 10px;
+            border-radius: 5px;
+            border: 1px solid;
+            margin-bottom: 10px;
+        }
+
+        input[type="submit"] {
+            background-color: #4CAF50;
+            color: white;
+            border: 1px solid;
+            padding: 10px 20px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 16px;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+
+        .center {
+            text-align: center;
+            margin-top: 20px;
+        }
+
+        .btn {
+            background-color: #008CBA;
+            color: white;
+            padding: 10px 20px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 16px;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+    </style>
+</head>
+<body>
+    <h2>Create User</h2>
+    <form method="POST" action="">
+        <label for="f_name">First Name:</label>
+        <input type="text" name="f_name" id="f_name" required>
+
+        <label for="l_name">Last Name:</label>
+        <input type="text" name="l_name" id="l_name" required>
+
+        <label for="email">Email:</label>
+        <input type="email" name="email" id="email" required>
+
+        <label for="passwd">Password:</label>
+        <input type="password" name="passwd" id="passwd" required>
+
+        <input type="submit" value="Create">
+    </form>
+    <div class="center">
+        <a href="http://127.0.0.1/Market/back/users/" class="btn">Volver</a>
+</div>
+
+</body>
+</html>
